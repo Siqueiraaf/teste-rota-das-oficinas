1479f2b01e4e19529885126e1ce1392901366854
Siqueiraaf <rafael.siqueira098@gmail.com>
2025-04-21 14:05:52 -0300

    feat(sale): add Sale and SaleItem entity to domain

---
+++ RO.DevTest.Domain/Entities/Sale.cs
@@ -0,0 +1,11 @@
+﻿namespace RO.DevTest.Domain.Entities;
+
+public class Sale
+{
+    public Guid Id { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public User User { get; set; } = null!; // admin ou customer
+    public decimal TotalAmount { get; set; }
+    public DateTime SaleDate { get; set; }
+    public ICollection<SaleItem> Items { get; set; } = new List<SaleItem>();
+}
