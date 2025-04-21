eca8632c8e51630b0ba919751fa76f678b86bd54
Siqueiraaf <rafael.siqueira098@gmail.com>
2025-04-21 14:16:36 -0300

    update DefaltContext for new DbSets

--- RO.DevTest.Persistence/DefaultContext.cs
+++ RO.DevTest.Persistence/DefaultContext.cs
@@ -10,6 +10,11 @@ public class DefaultContext : IdentityDbContext<User> {
 
     public DefaultContext(DbContextOptions<DefaultContext> options) : base(options) { }
 
+    // DbSets para as novas entidades
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<Sale> Sales { get; set; } = null!;
+    public DbSet<SaleItem> SaleItems { get; set; } = null!;
+
     protected override void OnModelCreating(ModelBuilder builder) {
         builder.HasPostgresExtension("uuid-ossp");
         builder.ApplyConfigurationsFromAssembly(typeof(DefaultContext).Assembly);
